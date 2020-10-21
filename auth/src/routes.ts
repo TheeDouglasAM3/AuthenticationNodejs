2f039fdbbfb75aebef0cacd6a3fdd0984d9e5535
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-21 19:05:41 -0300

    Add listUsers and session routes

--- auth/src/routes.ts
+++ auth/src/routes.ts
@@ -1,5 +1,7 @@
 import { Router, Response, Request } from 'express'
-import { saveUser } from './controller/UserController'
+import { saveUser, listUsers, login } from './controller/UserController'
+
+import { auth } from './middlewares/auth'
 
 const routes = Router()
 
@@ -7,6 +9,11 @@ routes.get('/', (request: Request, response: Response) => {
     return response.json({ message: 'oiiii'})
 })
 
+routes.post('/session', login)
+
+routes.use(auth)
+
+routes.get('/users', listUsers)
 routes.post('/users', saveUser)
 
 export default routes
