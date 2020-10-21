c0f167c29b06f8a05452c27eb97f730ec506470a
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-20 22:10:03 -0300

    Add saveUser function

--- auth/src/routes.ts
+++ auth/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router, Response, Request } from 'express'
+import { saveUser } from './controller/UserController'
 
 const routes = Router()
 
 routes.get('/', (request: Request, response: Response) => {
     return response.json({ message: 'oiiii'})
 })
+
+routes.post('/users', saveUser)
 
 export default routes
