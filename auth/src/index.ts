19b56900c22150726c58b7a5f9a60cbef67adca5
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-21 20:35:53 -0300

    Add cors in index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import './config/env'
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
+import * as cors from "cors"
 import routes from './routes'
 
 const app = express()
@@ -9,5 +10,6 @@ const app = express()
 createConnection()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.listen(3333)
